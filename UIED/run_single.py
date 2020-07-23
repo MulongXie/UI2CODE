37b32227bb16691f5a50785d31c88feae460e5c8
MulongXie <dsh15325@163.com>
2020-07-23 14:47:49 +1000

    rename

---
+++ UIED/run_single.py
@@ -0,0 +1,56 @@
+from os.path import join as pjoin
+import cv2
+import os
+
+
+def resize_height_by_longest_edge(img_path, resize_length=800):
+    org = cv2.imread(img_path)
+    height, width = org.shape[:2]
+    if height > width:
+        return resize_length
+    else:
+        return int(resize_length * (height / width))
+
+
+if __name__ == '__main__':
+
+    # set input image path
+    input_path_img = 'data/input/30800.jpg'
+    output_root = 'data/output'
+
+    resized_height = resize_height_by_longest_edge(input_path_img)
+
+    is_ip = True
+    is_clf = True
+    is_ocr = True
+    is_merge = True
+
+    if is_ocr:
+        import detect_text_east.ocr_east as ocr
+        import detect_text_east.lib_east.eval as eval
+        os.makedirs(pjoin(output_root, 'ocr'), exist_ok=True)
+        models = eval.load()
+        ocr.east(input_path_img, output_root, models,
+                 resize_by_height=resized_height, show=False)
+
+    if is_ip:
+        import detect_compo.ip_region_proposal as ip
+        os.makedirs(pjoin(output_root, 'ip'), exist_ok=True)
+        # switch of the classification func
+        classifier = None
+        if is_clf:
+            from cnn.CNN import CNN
+            classifier = {}
+            # classifier['Image'] = CNN('Image')
+            classifier['Elements'] = CNN('Elements')
+            # classifier['Noise'] = CNN('Noise')
+        ip.compo_detection(input_path_img, output_root, classifier=classifier,
+                           resize_by_height=resized_height, show=False)
+
+    if is_merge:
+        import merge
+        name = input_path_img.split('/')[-1][:-4]
+        compo_path = pjoin(output_root, 'ip', str(name) + '.json')
+        ocr_path = pjoin(output_root, 'ocr', str(name) + '.json')
+        merge.incorporate(input_path_img, compo_path, ocr_path, output_root,
+                          resize_by_height=resized_height, show=True)
