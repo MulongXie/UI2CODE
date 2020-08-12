a9b77a2a1ab21f1e953275b77260c55916b7d45d
MulongXie <dsh15325@163.com>
2020-08-12 10:53:07 +1000

    update main

--- Code-Generation/main.py
+++ Code-Generation/main.py
@@ -19,12 +19,17 @@ groups_nontext, no_groups_nontext = compos_nontext.cvt_groups('group_nontext', '
 groups_text, no_groups_text = compos_text.cvt_groups('group_text', 'text')
 all_groups = groups_nontext + groups_text
 
-# pair different repetitions to look for possible combination
+# pair differnet repetition to search for possible combination
 pairs, no_pairs = pair_matching_within_groups(all_groups)
-
-df_no_groups = no_groups_nontext.append(no_groups_text, sort=False)
-df_no_pairs = groups_cvt_df(no_pairs)
 df_pairs = pair_cvt_df(pairs)
 
+# collect all results in a Dataframe
+df_no_groups = no_groups_nontext.append(no_groups_text, sort=False)
+df_no_pairs = groups_cvt_df(no_pairs)
 df_all = df_no_groups.append(df_no_pairs, sort=False).append(df_pairs, sort=False)
+
+# post processing
 df_all = df_all.sort_index()
+df_all[list(df_all.filter(like='group'))] = df_all[list(df_all.filter(like='group'))].fillna(-1).astype(int)
+df_all['pair'] = df_all['pair'].fillna(-1).astype(int)
+draw.visualize_block(img, df_all, img_shape, attr='pair')
