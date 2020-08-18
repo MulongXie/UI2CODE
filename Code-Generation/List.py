05f0807eca583b825c53dc4ebc64c51bc91b700a
MulongXie <dsh15325@163.com>
2020-08-18 14:48:26 +1000

    update list

--- Code-Generation/List.py
+++ Code-Generation/List.py
@@ -20,37 +20,40 @@ def gather_list_items(compos):
         if compo['id'] not in mark and compo['pair_to'] not in mark:
             compo_paird = compos.loc[compo['pair_to']]
             item = ListItem(compo['alignment'], [compo, compo_paird])
+
             list_items[compo['id']] = item
-            mark += [compo['id'] , compo['pair_to']]
+            compos.loc[compo['id'], 'list'] = compo['id']
+            compos.loc[compo['pair_to'], 'list'] = compo['id']
+            mark += [compo['id'], compo['pair_to']]
 
         elif compo['id'] in mark and compo['pair_to'] not in mark:
             index = search_list_item_by_compoid(list_items, compo['id'])
             list_items[index].add_compo(compos.loc[compo['pair_to']])
+            compos.loc[compo['pair_to'], 'list'] = index
             mark.append(compo['pair_to'])
 
         elif compo['id'] not in mark and compo['pair_to'] in mark:
             index = search_list_item_by_compoid(list_items, compo['pair_to'])
             list_items[index].add_compo(compos.loc[compo['id']])
+            compos.loc[compo['id'], 'list'] = index
             mark.append(compo['id'])
         # print(compo['id'], compo['pair_to'])
+    compos['list'] = compos['list'].astype(int)
     return list_items
 
 
 class ListItem:
-    def __init__(self, alignment, compos, ids=None):
+    def __init__(self, alignment, compos):
         self.alignment = alignment
-        self.compos = compos
-        self.compos_id = ids if ids is not None else self.get_compos_id()
-
-    def get_compos_id(self):
-        return [compo['id'] for compo in self.compos]
+        self.compos = pd.DataFrame(compos)
+        self.compos_id = list(self.compos['id'])
 
     def add_compo(self, compo):
         self.compos.append(compo)
-        self.compos_id = self.get_compos_id()
+        self.compos_id = list(self.compos['id'])
 
     def visualize(self, img, img_shape, attr='class', name='board'):
-        draw.visualize(img, self.elements, img_shape, attr, name)
+        draw.visualize(img, self.compos, img_shape, attr, name)
 
     def visualize_block(self, img, img_shape, attr='class', name='board'):
-        draw.visualize_block(img, self.elements, img_shape, attr, name)
+        draw.visualize_block(img, self.compos, img_shape, attr, name)
