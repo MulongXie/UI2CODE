c3e7dc79fb59508fccc352a1b672ae1cb0d51189
MulongXie <dsh15325@163.com>
2020-08-18 11:49:03 +1000

    update block data structure

--- Code-Generation/Block.py
+++ Code-Generation/Block.py
@@ -3,9 +3,13 @@ import draw
 
 
 class Block:
-    def __init__(self, compos):
+    def __init__(self, block_id, compos, list_items=None):
+        self.id = block_id
         self.compos = compos
-        self.list_items = None
+        self.list_items = list_items if list_items is not None else []
+
+    def add_list_item(self, list_item):
+        self.list_items.append(list_item)
 
     def visualize(self, img, img_shape, attr='class', name='board'):
         draw.visualize(img, self.compos, img_shape, attr, name)
--- Code-Generation/List.py
+++ Code-Generation/List.py
@@ -2,13 +2,38 @@ import pandas as pd
 import draw
 
 
-def match_list_item_by_compoid(list_items, compo_id):
+def search_list_item_by_compoid(list_items, compo_id):
     """
         list_items: dictionary => {id of first compo: ListItem}
     """
     for i in list_items:
         if compo_id in list_items[i].compos_id:
             return i
+
+
+def gather_list_items(compos):
+    list_items = {}
+    mark = []
+    for i in range(len(compos)):
+        compo = compos.iloc[i]
+        # new item
+        if compo['id'] not in mark and compo['pair_to'] not in mark:
+            compo_paird = compos.loc[compo['pair_to']]
+            item = ListItem(compo['alignment'], [compo, compo_paird])
+            list_items[compo['id']] = item
+            mark += [compo['id'] , compo['pair_to']]
+
+        elif compo['id'] in mark and compo['pair_to'] not in mark:
+            index = search_list_item_by_compoid(list_items, compo['id'])
+            list_items[index].add_compo(compos.loc[compo['pair_to']])
+            mark.append(compo['pair_to'])
+
+        elif compo['id'] not in mark and compo['pair_to'] in mark:
+            index = search_list_item_by_compoid(list_items, compo['pair_to'])
+            list_items[index].add_compo(compos.loc[compo['id']])
+            mark.append(compo['id'])
+        # print(compo['id'], compo['pair_to'])
+    return list_items
 
 
 class ListItem:
