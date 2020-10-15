e151fded41d7d5d7de9898f77a6160cb450d0b81
MulongXie <dsh15325@163.com>
2020-10-15 17:27:43 +1100

    add split_grp_with_intersection and find_sandwich

--- Code-Generation/v7/obj/Group.py
+++ Code-Generation/v7/obj/Group.py
@@ -1,9 +1,62 @@
 
+def split_groups_with_intersection(compos):
+    grps = {}
+    intersections = {}
+    groups = compos.groupby('group').groups
+    for i in groups:
+        if len(groups[i]) <= 1:
+            continue
+        grp = Group(i, compos.loc[groups[i]])
+        for j in grps:
+            g = grps[j]
+            if grp.is_intersected(g):
+                if grp.id not in intersections:
+                    intersections[grp.id] = [g.id]
+                else:
+                    intersections[grp.id].append(g.id)
+                if g.id not in intersections:
+                    intersections[g.id] = [grp.id]
+                else:
+                    intersections[g.id].append(grp.id)
+        grps[grp.id] = grp
+    return grps, intersections
+
+
+def find_sandwiches_in_group(group, compos_all):
+    alignment = group.alignment
+    sandwiches = []
+    for i in range(len(group.compos) - 1):
+        c_a = group.compos.iloc[i]
+        c_b = group.compos.iloc[i + 1]
+        col_min = min(c_a['column_min'], c_b['column_min'])
+        col_max = max(c_a['column_max'], c_b['column_max'])
+        row_min = min(c_a['row_min'], c_b['row_min'])
+        row_max = max(c_a['row_max'], c_b['row_max'])
+        sandwich = compos_all[compos_all['group'] != group.id]
+        if alignment == 'h':
+            # filter elements not on the same horizontal level
+            sandwich = sandwich[~((sandwich['row_max'] < row_min) | (sandwich['row_min'] > row_max))]
+            # c_a is on the left of c_b
+            sandwich = sandwich[
+                (sandwich['column_min'] > c_a['column_max']) & (sandwich['column_max'] < c_b['column_min'])]
+        elif alignment == 'v':
+            # filter elements not on the same vertical level
+            sandwich = sandwich[~((sandwich['column_max'] < col_min) | (sandwich['column_min'] > col_max))]
+            # c_a is above c_b
+            sandwich = sandwich[(sandwich['row_min'] > c_a['row_max']) & (sandwich['row_max'] < c_b['row_min'])]
+        sandwiches.append(sandwich)
+    return sandwiches
+
+
 class Group:
     def __init__(self, id, compos):
         self.id = id
         self.compos = compos                                    # dataframe
         self.alignment = compos.iloc[0]['alignment_in_group']   # group element's alignment
+        if self.alignment == 'v':
+            self.compos.sort_values('row_min', inplace=True)
+        else:
+            self.compos.sort_values('column_min', inplace=True)
 
         self.top, self.bottom, self.left, self.right = self.group_boundary()
         self.width = self.bottom - self.top
